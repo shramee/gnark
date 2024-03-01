941d604902eb76ba928fe5129d7d229a702a3149
Shramee Srivastav <shramee.srivastav@gmail.com>
2024-03-01 06:54:38 +0530

    Karabina 2345 test

--- std/algebra/emulated/fields_bn254/e12_test.go
+++ std/algebra/emulated/fields_bn254/e12_test.go
@@ -1,13 +1,16 @@
 package fields_bn254
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark-crypto/ecc/bn254"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fp"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/std/math/emulated"
 	"github.com/consensys/gnark/test"
+	"github.com/stretchr/testify/assert"
 )
 
 type e12Add struct {
@@ -219,6 +222,164 @@ func (circuit *e12CycloSquareKarabina) Define(api frontend.API) error {
 	expected := e.CyclotomicSquareCompressed(&circuit.A)
 	e.AssertIsEqual(expected, &circuit.C)
 	return nil
+}
+
+func PrintFq12(x *bn254.E12) {
+	fmt.Printf("fq12(\n0x%v,\n0x%v,\n0x%v,\n0x%v,\n0x%v,\n0x%v,\n0x%v,\n0x%v,\n0x%v,\n0x%v,\n0x%v,\n0x%v,\n)\n",
+		x.C0.B0.A0.Text(16),
+		x.C0.B0.A1.Text(16),
+		x.C0.B1.A0.Text(16),
+		x.C0.B1.A1.Text(16),
+		x.C0.B2.A0.Text(16),
+		x.C0.B2.A1.Text(16),
+		x.C1.B0.A0.Text(16),
+		x.C1.B0.A1.Text(16),
+		x.C1.B1.A0.Text(16),
+		x.C1.B1.A1.Text(16),
+		x.C1.B2.A0.Text(16),
+		x.C1.B2.A1.Text(16),
+	)
+}
+func PrintFq12Code(x *bn254.E12) {
+	fmt.Printf(
+		`
+		x.C0.B0.A0 = fp.Element{%v};
+		x.C0.B0.A0 = fp.Element{%v};
+		x.C0.B0.A0 = fp.Element{%v};
+		x.C0.B0.A0 = fp.Element{%v};
+		x.C0.B0.A0 = fp.Element{%v};
+		x.C0.B0.A0 = fp.Element{%v};
+		x.C0.B0.A0 = fp.Element{%v};
+		x.C0.B0.A0 = fp.Element{%v};
+		x.C0.B0.A0 = fp.Element{%v};
+		x.C0.B0.A0 = fp.Element{%v};
+		x.C0.B0.A0 = fp.Element{%v};
+		x.C0.B0.A0 = fp.Element{%v};
+	`,
+		x.C0.B0.A0,
+		x.C0.B0.A1,
+		x.C0.B1.A0,
+		x.C0.B1.A1,
+		x.C0.B2.A0,
+		x.C0.B2.A1,
+		x.C1.B0.A0,
+		x.C1.B0.A1,
+		x.C1.B1.A0,
+		x.C1.B1.A1,
+		x.C1.B2.A0,
+		x.C1.B2.A1,
+	)
+}
+
+func PrintFq2(x *bn254.E2) {
+	fmt.Printf(
+		"fq2(\n0x%v,\n0x%v\n)\n",
+		x.A0.Text(16),
+		x.A1.Text(16),
+	)
+}
+
+func PrintFq6(x *bn254.E6) {
+	fmt.Printf(
+		"fq6(\n0x%v,\n0x%v,\n0x%v,\n0x%v,\n0x%v,\n0x%v\n)\n",
+		x.B0.A0.Text(16),
+		x.B0.A1.Text(16),
+		x.B1.A0.Text(16),
+		x.B1.A1.Text(16),
+		x.B2.A0.Text(16),
+		x.B2.A1.Text(16),
+	)
+}
+
+func TestShramee(t *testing.T) {
+	var a, sqr bn254.E12
+	var S1, S2, S3, S4, S5, S4_5, S2_3 bn254.E2
+	var g1, g2, g3, g4, g5, temp bn254.E2
+	_, _ = a.SetRandom()
+
+	temp.Set(&a.C0.B0) // Dummy operation to avoid compiler optimization
+
+	a.C0.B0.A0 = fp.Element{17794508311707236187, 14332130291088027409, 12327615803937871582, 1672598124401330291}
+	a.C0.B0.A1 = fp.Element{14683273760673331958, 13235901128822352485, 674669255719435283, 2527920418959154559}
+	a.C0.B1.A0 = fp.Element{11717593603483070296, 3765665073603391747, 4379395531500719051, 2571800391257408738}
+	a.C0.B1.A1 = fp.Element{8799594128983248235, 16048392343996365614, 5294554140119399641, 375642032802126072}
+	a.C0.B2.A0 = fp.Element{13510475362672197881, 17651724906677260323, 4747476604120526089, 942393905552006150}
+	a.C0.B2.A1 = fp.Element{1027419623891463186, 7995939757302718278, 2061172121645149172, 12637001424321839}
+	a.C1.B0.A0 = fp.Element{2511833691738056164, 1250461804511819271, 822745160128054571, 164647970030674767}
+	a.C1.B0.A1 = fp.Element{6414597929097366183, 1367166416163732384, 2761718821210626786, 198047499578225569}
+	a.C1.B1.A0 = fp.Element{2652703260343647500, 3952924384317760951, 16258367926645146318, 3168252663914322598}
+	a.C1.B1.A1 = fp.Element{4420421771147043633, 50160570955363120, 15009854162151977803, 2237888184795023069}
+	a.C1.B2.A0 = fp.Element{17373359534443121793, 13688175116481631289, 16073045774962078384, 2630857964147183816}
+	a.C1.B2.A1 = fp.Element{11747177762025469378, 586822099072867711, 6784825836541080345, 3309921329095231725}
+
+	// put a in the cyclotomic subgroup
+	// var tmp bn254.E12
+	// tmp.Conjugate(&a)
+	// a.Inverse(&a)
+	// tmp.Mul(&tmp, &a)
+	// a.FrobeniusSquare(&tmp).Mul(&a, &tmp)
+	// fmt.Println("fn cyclotomic_input() -> Fq12 {")
+	// PrintFq12(&a)
+	// fmt.Print("}\n\n")
+
+	// // t4 = nr * g5^2
+	// h2.MulByNonResidue(&S5)
+
+	// // temp = nr * g5^2 + g1^2
+	// temp.Add(&S1, &h2)
+
+	// // t6 = nr * g5^2 + g1^2 - g2
+	// h2.Sub(&temp, &g2)
+	// // t6 = 2 * nr * g5^2 + 2 * g1^2 - 2*g2
+	// h2.Double(&h2)
+	// // z2 = 3 * nr * g5^2 + 3 * g1^2 - 2*g2
+	// h2.Add(&h2, &temp)
+
+	// PrintFq2(&h2)
+
+	sqr.Square(&a)
+	g1 = a.C0.B1
+	g2 = a.C0.B2
+	g3 = a.C1.B0
+	g4 = a.C1.B1
+	g5 = a.C1.B2
+
+	S1.Square(&g1)
+	S2.Square(&g2)
+	S3.Square(&g3)
+	S4.Square(&g4)
+	S5.Square(&g5)
+	S4_5.Square(S4_5.Add(&g4, &g5))
+	S2_3.Square(S2_3.Add(&g2, &g3))
+
+	PrintFq2(&sqr.C0.B2)
+	var h2 bn254.E2
+
+	// Karabina 2345 cyclotomic square h2
+	// h2 = 3(S4_5 − S4 − S5)ξ + 2g2;
+	temp.Sub(temp.Sub(&S4_5, &S4), &S5) // S4_5 − S4 − S5
+	temp.MulByNonResidue(&temp)         // (S4_5 − S4 − S5)ξ
+	h2.Add(&temp, &g2)                  // (S4_5 − S4 − S5)ξ + g2
+	h2.Add(&h2, &h2)                    // 2(S4_5 − S4 − S5)ξ + 2g2
+	h2.Add(&h2, &temp)                  // 2(S4_5 − S4 − S5)ξ + 2g2 + (S4_5 − S4 − S5)ξ
+
+	PrintFq2(&h2)
+
+	// // Print cyclotomic square
+	// sqr.Square(&a);
+	// fmt.Println("fn sqr() -> Fq12 {")
+	// PrintFq12(&sqr);
+	// fmt.Print("}\n\n")
+
+	// Print compressed Karabina 2345 cyclotomic square
+	// var k_sqr bn254.E12
+	// k_sqr.CyclotomicSquareCompressed(&a)
+	// fmt.Println("fn karabina_square() -> Fq12 {")
+	// PrintFq12(&k_sqr)
+	// fmt.Print("}\n\n")
+
+	assert.Equal(t, h2.A0.Text(16), sqr.C0.B2.A0.Text(16))
+	assert.Equal(t, h2.A1.Text(16), sqr.C0.B2.A1.Text(16))
 }
 
 func TestFp12CyclotomicSquareKarabina(t *testing.T) {
